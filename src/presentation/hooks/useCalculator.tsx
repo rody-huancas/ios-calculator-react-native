ec239744d2f789b649ff72b99cd8c8bbbe6aa755
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-04 18:25:05 -0500

    Feat: useCalculator

---
+++ src/presentation/hooks/useCalculator.tsx
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+
+export const useCalculator = () => {
+  const [number, setNumber] = useState('0');
+
+  const buildNumber = (numberString : string) => {
+    if ( number.includes('.') && numberString === '.' ) return;
+
+    if ( number.startsWith('0') || number.startsWith('-0') ) {
+      // Punto decimal
+      if ( numberString === '.' ) {
+        return setNumber(number + numberString);
+      }
+
+      // Evaluar si es otro cero y no hay punto
+      if ( numberString === '0' && number.includes('.') ) {
+        return setNumber(number + numberString);
+      }
+
+      // Evaluar si es diferente de cero, no hay punto, y es el primer número
+      if ( numberString !== '0' && !number.includes('.') ) {
+        return setNumber(numberString);
+      }
+
+      // Evitar 00000.00
+      if ( numberString === '0' && !number.includes('.') ) return;
+
+      return setNumber(number + numberString)
+    }
+
+    setNumber(number + numberString);
+  }
+
+  return {
+    // Properties
+    number,
+    // Methods
+    buildNumber
+  };
+};
